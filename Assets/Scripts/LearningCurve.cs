c6aaff509e5588690ab552bc3921e98784db7a4a
Mike Leys <mike@leys.dev>
2023-08-23 15:22:17 -0400

    Test Script

---
+++ Assets/Scripts/LearningCurve.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    public int CurrentAge = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.log(30 + 1);
+        Debug.log(CurrentAge + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
